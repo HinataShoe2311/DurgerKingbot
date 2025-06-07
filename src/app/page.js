e5f167d6b6d5ff0ce26e15fd9c8614b3b75538de
Manoj Sharma <mmmanojsharma201611@gmail.com>
2025-06-07 07:54:41 +0000

    refactoring

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
 "use client";
-import Image from "next/image";
 import FoodPage from "@/Components/FoodPage";
 export default function Home() {
   return (
-    <div className="grid  items-center justify-items-center min-h-screen   font-[family-name:var(--font-geist-sans)]">
+    <div className="grid items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <FoodPage />
       </main>
